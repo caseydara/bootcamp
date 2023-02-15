b2062d49a4c81e13090ac5bba539a51ddc4c0def
Dara Casey <dara.e.casey.25@dartmouth.edu>
2023-02-15 18:20:06 -0500

    quizlet next + previous buttons working

--- src/CardViewer.js
+++ src/CardViewer.js
@@ -1,63 +1,27 @@
-// import React from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 
 function CardViewer(props) {
-  let front = true;
-  let currIndex = 0;
+  const [front, setFront] = useState(true);
+  const [currIndex, setCurrIndex] = useState(0);
 
-  const switchCard = () => {
-    if (front) {
-      document.getElementById('card').innerHTML = props.cards.slice()[currIndex].front;
-    } else {
-      document.getElementById('card').innerHTML = props.cards.slice()[currIndex].back;
-    }
-  };
   const switchSide = () => {
-    front = !front;
-    switchCard();
+    setFront(!front);
   };
   const newCard = (index) => {
-    // if (index < 0 || index > props.cards.slice().length - 1) {
-    //   return;
-    // }
-    const previous = document.getElementById('previous');
-    const next = document.getElementById('next');
-    if (index === 0) {
-      next.disabled = false;
-      previous.disabled = true;// false
-    } else if (index === props.cards.slice().length - 1) {
-      next.disabled = true;
-      previous.disabled = false;
-    } else {
-      next.disabled = false;
-      previous.disabled = false;
-    }
-    currIndex = index;
-    front = true;
-    switchCard();
-    document.getElementById('progress').innerHTML = `Card ${currIndex + 1}/${props.cards.slice().length}`;
+    setCurrIndex(index);
+    setFront(true);
   };
 
   return (
     <div>
       <h2>Card Viewer</h2>
-      <h3 id="progress">Card 1/{props.cards.slice().length}</h3>
-      <div id="card" onClick={() => switchSide()}>{props.cards.slice()[currIndex].front}</div>
-      <button id="previous" type="button" onClick={() => newCard(currIndex - 1)}>Previous</button>
-      <button id="next" type="button" onClick={() => newCard(currIndex + 1)}>Next</button>
+      <h3 id="progress">Card {currIndex + 1}/{props.cards.slice().length}</h3>
+      <div id="card" onClick={() => switchSide()}>{front ? props.cards.slice()[currIndex].front : props.cards.slice()[currIndex].back}</div>
+      <button id="previous" disabled={currIndex === 0} type="button" onClick={() => newCard(currIndex - 1)}>Previous</button>
+      <button id="next" disabled={currIndex === props.cards.slice().length - 1} type="button" onClick={() => newCard(currIndex + 1)}>Next</button>
       <button type="button" onClick={() => props.switchMode()}>switch mode</button>
     </div>
   );
 }
-
-// class CardViewer extends React.Component {
-//     render(){
-//         return(
-//             <div>
-//                 <h2>Card Viewer</h2>
-//             </div>
-//         );
-//     }
-// }
 
 export default CardViewer;
